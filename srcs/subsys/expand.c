41c8707e1d59a68daa6d96a9204f86b23464832f
sielee <lalala97557@gmail.com>
2022-07-25 18:23:54 +0900

    sub

--- srcs/subsys/expand.c
+++ srcs/subsys/expand.c
@@ -1,26 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   expand.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sielee <sielee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/13 17:12:35 by sielee            #+#    #+#             */
-/*   Updated: 2022/07/25 18:14:53 by sielee           ###   ########seoul.kr  */
+/*   Created: 2022/07/25 18:21:58 by sielee            #+#    #+#             */
+/*   Updated: 2022/07/25 18:23:26 by sielee           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
-
-# include "../libft/libft.h"
-# include "parser.h"
-# include "executor.h"
-# include <readline/readline.h>// for readline fnc call
-# include <readline/history.h>
-# include "basic_utils.h"
-
-#include <stdio.h> //나중에 지지워워!
-
-
-#endif
+#include "minishell.h"
