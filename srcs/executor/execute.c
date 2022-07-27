575e98083bf14c48ddbe517fd060b52223ef496a
sielee <lalala97557@gmail.com>
2022-07-27 22:49:47 +0900

    sdf

--- srcs/executor/execute.c
+++ srcs/executor/execute.c
@@ -6,9 +6,50 @@
 /*   By: sielee <sielee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 18:11:51 by sielee            #+#    #+#             */
-/*   Updated: 2022/07/25 18:24:45 by sielee           ###   ########seoul.kr  */
+/*   Updated: 2022/07/27 20:25:57 by sielee           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	ft_exit_stat(int status)
+{
+	const int	w_status = status & 0177;
+
+	if (w_status == 0)
+		return ((status >> 8) & 0x000000ff);
+	if (w_status != 0177 && w_status != 0)
+		return (w_status);
+	return (0);
+}
+
+void	ft_execute_cmd()
+{
+	//path찾기
+	//execve(,, envp);
+}
+
+void	ft_process()
+{
+
+	ft_execute_cmd();
+}
+
+int	ft_executor()
+{
+	pid_t	pid;
+	int		status;
+
+	while ()//실행가능단위를 다 실행할때까지
+	{
+		pid = fork();
+		if (pid == -1)
+			ft_error("fork error");
+		else
+		{
+			
+		}
+	}
+	return (ft_exit_stat(status));
+}
 
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: sielee <sielee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/13 17:12:20 by sielee            #+#    #+#             */
-/*   Updated: 2022/07/25 20:16:27 by sielee           ###   ########seoul.kr  */
+/*   Updated: 2022/07/27 20:22:06 by sielee           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 int	main(int ac, char *av[], char **envp)
 {
-	int ret;
+	int ret;//구조체 큰거 하나에 담는게 좋을듯
 
 	if (ac > 1)
 		ft_error("Wrong Argc\n");
+	//init: 인자 담기 및 파싱
+	//minishell_loop: readline > parse > execute
 	ret = ;
 	return (ret);
 }
