3d3572572bc6bc4c74267848fdbc256ad0001684
윤인성 <yun-inseong@yun-inseong-ui-MacBookAir.local>
2022-07-28 22:03:02 +0900

    ead

--- inc/parser.h
+++ inc/parser.h
@@ -13,4 +13,19 @@
 #ifndef PARSER_H
 # define PARSER_H
 
+typedef struct s_token
+{
+	char			*content;
+	struct s_token	*next;
+}	t_token;
+
+
+typedef struct s_lexer
+{
+	s_token		*head;
+	s_token		*tail;
+	char		*input_line;
+}	t_lexer;
+
+
 #endif
