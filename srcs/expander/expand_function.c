2d414290c464f144245e67b88bcb793fbc4621c2
윤인성 <yun-inseong@yun-inseong-ui-MacBookAir.local>
2022-08-15 20:53:09 +0900

    ex

--- srcs/expander/expand_function.c
+++ srcs/expander/expand_function.c
@@ -38,6 +38,17 @@ void	new_save(t_buffer **buffer)
 	}
 }
 
+void	ft_dollar_sub(t_buffer **buffer)
+{
+	if (ft_check_type(((*buffer)->content)[(*buffer)->index]) != ST_NULL)
+	{
+		(*buffer)->content += (*buffer)->index;
+		(*buffer)->index = 1;
+	}
+	else if (ft_check_type(((*buffer)->content)[(*buffer)->index]) == ST_NULL)
+		(*buffer)->curr_state = EX_NULL;
+}
+
 void	ft_dollar(t_buffer **buffer)
 {
 	char			*temp;
@@ -62,13 +73,7 @@ void	ft_dollar(t_buffer **buffer)
 		}
 		temp2 = temp2->next;
 	}
-	if (ft_check_type(((*buffer)->content)[(*buffer)->index]) != ST_NULL)
-	{
-		(*buffer)->content += (*buffer)->index;
-		(*buffer)->index = 1;
-	}
-	else if (ft_check_type(((*buffer)->content)[(*buffer)->index]) == ST_NULL)
-		(*buffer)->curr_state = EX_NULL;
+	ft_dollar_sub(buffer);
 	free(temp);
 }
 
@@ -102,6 +107,27 @@ int	ft_ex_norm_to_dq(t_buffer *buffer)
 	return (0);
 }
 
+void	ft_ex_norm_to_dollar_sub(t_buffer *buffer)
+{
+	if (ft_check_type(*(buffer->content)) == SINGLE_QUOTE)
+	{
+		buffer->curr_state = EX_SI_QUO;
+		buffer->content++;
+		buffer->index = 0;
+	}
+	else if (ft_check_type(*(buffer->content)) == DOUBLE_QUOTE)
+	{
+		buffer->curr_state = EX_DO_QUO;
+		buffer->content++;
+		buffer->index = 0;
+	}
+	else if (*(buffer->content) == '\\')
+	{
+		buffer->content++;
+		buffer->index = 0;
+	}
+}
+
 int	ft_ex_norm_to_dollar(t_buffer *buffer)
 {
 	if (check_meta((buffer->content)[buffer->index + 1]) == EX_DOLLAR)
@@ -122,26 +148,30 @@ int	ft_ex_norm_to_dollar(t_buffer *buffer)
 			while (check_meta((buffer->content)[buffer->index]) == EX_NORMAL)
 				buffer->index++;
 			ft_dollar(&buffer);
-			if (ft_check_type(*(buffer->content)) == SINGLE_QUOTE)
-			{
-				buffer->curr_state = EX_SI_QUO;
-				buffer->content++;
-				buffer->index = 0;
-			}
-			else if (ft_check_type(*(buffer->content)) == DOUBLE_QUOTE)
-			{
-				buffer->curr_state = EX_DO_QUO;
-				buffer->content++;
-				buffer->index = 0;
-			}
-			else if (*(buffer->content) == '\\')
-			{
-				buffer->content++;
-				buffer->index = 0;
-			}
+			ft_ex_norm_to_dollar_sub(buffer);
 		}
 	}
 	return (0);
+}
+
+void	ft_ex_qou_to_dollar_sub(t_buffer *buffer)
+{
+	while (check_meta((buffer->content)[buffer->index]) == EX_NORMAL)
+		buffer->index++;
+	ft_dollar(&buffer);
+	if (ft_check_type(*(buffer->content)) == DOUBLE_QUOTE)
+	{
+		buffer->curr_state = EX_NORMAL;
+		if (ft_check_type((buffer->content)[buffer->index]) != ST_NULL)
+		{
+			buffer->content += 1;
+			buffer->index = 0;
+		}
+		else if (ft_check_type((buffer->content)[buffer->index]) == ST_NULL)
+			buffer->curr_state = EX_NULL;
+	}
+	else
+		buffer->index = 0;
 }
 
 int	ft_ex_qou_to_dollar(t_buffer *buffer)
@@ -160,24 +190,7 @@ int	ft_ex_qou_to_dollar(t_buffer *buffer)
 			buffer->index = 0;
 		}
 		else
-		{
-			while (check_meta((buffer->content)[buffer->index]) == EX_NORMAL)
-				buffer->index++;
-			ft_dollar(&buffer);
-			if (ft_check_type(*(buffer->content)) == DOUBLE_QUOTE)
-			{
-				buffer->curr_state = EX_NORMAL;
-				if (ft_check_type((buffer->content)[buffer->index]) != ST_NULL)
-				{
-					buffer->content += 1;
-					buffer->index = 0;
-				}
-				else if (ft_check_type((buffer->content)[buffer->index]) == ST_NULL)
-					buffer->curr_state = EX_NULL;
-			}
-			else
-				buffer->index = 0;
-		}
+			ft_ex_qou_to_dollar_sub(buffer);
 	}
 	return (0);
 }
@@ -194,6 +207,31 @@ int	ft_ex_quo_to_quo(t_buffer *buffer)
 	else if (ft_check_type((buffer->content)[buffer->index]) == ST_NULL)
 		buffer->curr_state = EX_NULL;
 	return (0);
+}
+
+void	ft_ex_dollar_to_sub(t_buffer *buffer)
+{
+	if (ft_check_type(*(buffer->content)) == SINGLE_QUOTE)
+	{
+		buffer->curr_state = EX_SI_QUO;
+		buffer->content++;
+		buffer->index = 0;
+	}
+	else if (ft_check_type(*(buffer->content)) == DOUBLE_QUOTE)
+	{
+		buffer->curr_state = EX_DO_QUO;
+		buffer->content++;
+		buffer->index = 0;
+	}
+	else if (check_meta(*(buffer->content)) == EX_META)
+	{
+		buffer->curr_state = EX_NORMAL;
+		if (*(buffer->content) == '\\')
+		{
+			buffer->content++;
+			buffer->index = 0;
+		}
+	}
 }
 
 int	ft_ex_dollar_to(t_buffer *buffer)
@@ -219,27 +257,7 @@ int	ft_ex_dollar_to(t_buffer *buffer)
 			while (check_meta((buffer->content)[buffer->index]) == EX_NORMAL)
 				buffer->index++;
 			ft_dollar(&buffer);
-			if (ft_check_type(*(buffer->content)) == SINGLE_QUOTE)
-			{
-				buffer->curr_state = EX_SI_QUO;
-				buffer->content++;
-				buffer->index = 0;
-			}
-			else if (ft_check_type(*(buffer->content)) == DOUBLE_QUOTE)
-			{
-				buffer->curr_state = EX_DO_QUO;
-				buffer->content++;
-				buffer->index = 0;
-			}
-			else if (check_meta(*(buffer->content)) == EX_META)
-			{
-				buffer->curr_state = EX_NORMAL;
-				if (*(buffer->content) == '\\')
-				{
-					buffer->content++;
-					buffer->index = 0;
-				}
-			}
+			ft_ex_dollar_to_sub(buffer);
 		}
 	}
 	return (0);
