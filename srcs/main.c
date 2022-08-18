9526f373235f8f3f18ef655dbd65002c1b6903cb
윤인성 <yun-inseong@yun-inseong-ui-MacBookAir.local>
2022-08-18 17:40:52 +0900

    ex

--- srcs/main.c
+++ srcs/main.c
@@ -90,7 +90,7 @@ int	ft_minishell(t_envp_list *env)
 		add_history(line);
 		ft_parse(&pipe_list, env, line);
 		// ft_print(&pipe_list);//for testing
-		// g_exit_status = ft_execute(&pipe_list, env);
+		g_exit_status = ft_execute(&pipe_list, env);
 		ft_parser_free(&pipe_list);
 		ft_free((void **) &line);
 	}
