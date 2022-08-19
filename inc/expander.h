efb86e981d34bc069e778b5826b8928eefce878e
Insung Yun <iyun@c1r3s6.42seoul.kr>
2022-08-19 15:20:29 +0900

    rx

--- inc/expander.h
+++ inc/expander.h
@@ -16,16 +16,16 @@
 
 typedef struct s_buffer	t_buffer;
 
-typedef int		(*t_fuc_exp)(t_buffer *buffer);
+typedef int				(*t_fuc_exp)(t_buffer *buffer);
 
 typedef enum e_expand_state
 {
 	EX_NORMAL = 0,
 	EX_SI_QUO,
 	EX_DO_QUO,
-    EX_DOLLAR,
+	EX_DOLLAR,
 	EX_META,
-    EX_NULL
+	EX_NULL
 }	t_expand_state;
 
 typedef struct s_buffer
@@ -43,36 +43,40 @@ typedef struct s_fuc
 }	t_fuc;
 
 t_expand_state	check_expand_type(char c);
-t_expand_state check_meta(char c);
-void	new_save(t_buffer **buffer);
-void	ft_dollar(t_buffer **buffer);
-int	ft_ex_next_char(t_buffer *buffer);
-int	ft_ex_norm_to_sq(t_buffer *buffer);
-int	ft_ex_norm_to_dq(t_buffer *buffer);
-int	ft_ex_norm_to_dollar(t_buffer *buffer);
-int	ft_ex_qou_to_dollar(t_buffer *buffer);
-int	ft_ex_quo_to_quo(t_buffer *buffer);
-void	ft_expander_table(t_fuc *funct);
-void	ft_expander_arg(t_arg_node	**node, t_envp_list *list, t_fuc funct);
-void	ft_expand(t_pipe_list *pipe, t_envp_list *env);
-int	ft_ex_dollar_to(t_buffer *buffer);
-void	ft_question_mark(t_buffer *buffer);
-void	ft_expander_init(char *str, t_buffer **buffer, t_envp_list *list);
-int	buffer_init(char *str, t_buffer **buffer, t_envp_list *list);
-void	ft_que_init_sub(t_buffer *buffer);
-void	ft_que_init(t_buffer *buffer);
-t_expand_state check_meta_qu(char c);
+t_expand_state	check_meta(char c);
+void			new_save(t_buffer **buffer);
+void			ft_dollar(t_buffer **buffer);
+int				ft_ex_next_char(t_buffer *buffer);
+int				ft_ex_norm_to_sq(t_buffer *buffer);
+int				ft_ex_norm_to_dq(t_buffer *buffer);
+int				ft_ex_norm_to_dollar(t_buffer *buffer);
+int				ft_ex_qou_to_dollar(t_buffer *buffer);
+int				ft_ex_quo_to_quo(t_buffer *buffer);
+void			ft_expander_table(t_fuc *funct);
+void			ft_expander_arg(t_arg_node	**node, t_envp_list *list, \
+				t_fuc funct);
+void			ft_expand(t_pipe_list *pipe, t_envp_list *env);
+int				ft_ex_dollar_to(t_buffer *buffer);
+void			ft_question_mark(t_buffer *buffer);
+void			ft_expander_init(char *str, t_buffer **buffer, \
+				t_envp_list *list);
+int				buffer_init(char *str, t_buffer **buffer, t_envp_list *list);
+void			ft_que_init_sub(t_buffer *buffer);
+void			ft_que_init(t_buffer *buffer);
+t_expand_state	check_meta_qu(char c);
 t_expand_state	check_expand_lim_type(char c);
-void	ft_expander_arg(t_arg_node **node, t_envp_list *list, t_fuc funct);
-void	ft_expander_lim(t_limiter_node **node, t_fuc funct);
-void	ft_expander_redir(t_redir_node **node, t_envp_list *list, t_fuc funct);
-void	ft_del_node_arg(t_arg_node **temp_arg, t_arg_node **front);
-void	ft_del_node_redir(t_redir_node **temp_arg, t_redir_node **front);
+void			ft_expander_arg(t_arg_node **node, t_envp_list *list, \
+				t_fuc funct);
+void			ft_expander_lim(t_limiter_node **node, t_fuc funct);
+void			ft_expander_redir(t_redir_node **node, t_envp_list *list, \
+				t_fuc funct);
+void			ft_del_node_arg(t_arg_node **temp_arg, t_arg_node **front);
+void			ft_del_node_redir(t_redir_node **temp_arg, \
+				t_redir_node **front);
 t_expand_state	check_expand_heredoc_type(char c);
-void	ft_expander_heredoc(char **str, t_envp_list *list, t_fuc funct);
-
-int	ft_ex_norm_to_dollar_heredoc(t_buffer *buffer);
-int	ft_ex_dollar_to_heredoc(t_buffer *buffer);
-void	ft_expander_heredoc_table(t_fuc *funct);
+void			ft_expander_heredoc(char **str, t_envp_list *list, t_fuc funct);
+int				ft_ex_norm_to_dollar_heredoc(t_buffer *buffer);
+int				ft_ex_dollar_to_heredoc(t_buffer *buffer);
+void			ft_expander_heredoc_table(t_fuc *funct);
 
 #endif
--- inc/parser.h
+++ inc/parser.h
@@ -16,14 +16,13 @@
 
 typedef struct s_lexer		t_lexer;
 
-typedef int		(*t_function)(t_lexer *lexer);
+typedef int					(*t_function)(t_lexer *lexer);
 
 typedef enum e_qoute_state
 {
 	QOUTE_ON = 0,
 	QOUTE_OFF
 }	t_qoute_state;
-
 
 typedef enum e_meta
 {
@@ -144,10 +143,10 @@ typedef struct s_arg_node
 
 typedef struct s_pipe_node
 {
-	t_redir_list	*redir_list;
-	t_limiter_q		*lim_q;
-	t_arg_list		*arg_list;
-	t_envp_list		*env_list;
+	t_redir_list		*redir_list;
+	t_limiter_q			*lim_q;
+	t_arg_list			*arg_list;
+	t_envp_list			*env_list;
 	struct s_pipe_node	*next;
 }				t_pipe_node;
 
@@ -166,75 +165,72 @@ typedef struct s_action_state
 	struct s_action_state	*next;
 }	t_action_state;
 
-
-t_meta	ft_check_meta(char c);
-t_state	ft_check_type(char c);
-t_token_type		choose_type(t_token *new);
-t_token	*new_content(t_lexer *lexer);
-int	ft_next_char(t_lexer *lexer);
-int	ft_norm_to_blank(t_lexer *lexer);
-int	ft_norm_to_sq(t_lexer *lexer);
-int	ft_norm_to_dq(t_lexer *lexer);
-int	ft_norm_to_meta(t_lexer *lexer);
-int	ft_blank_to_norm(t_lexer *lexer);
-int	ft_blank_to_sq(t_lexer *lexer);
-int	ft_blank_to_dq(t_lexer *lexer);
-int	ft_blank_to_meta(t_lexer *lexer);
-int	ft_quote_to_quote(t_lexer *lexer);
-int	ft_meta_to_norm(t_lexer *lexer);
-int	ft_meta_to_blank(t_lexer *lexer);
-int	ft_meta_to_sq(t_lexer *lexer);
-int	ft_meta_to_dq(t_lexer *lexer);
-int	ft_meta_to_meta(t_lexer *lexer);
-void	lexer_setting(t_lexer *lexer, char *input_line);
-int ft_lexer(t_lexer *lexer);
-void	lexer_setting(t_lexer *lexer, char *input_line);
-int ft_lexer(t_lexer *lexer);
-void	ft_table(t_lexer *lexer);
-
-
-void	reduce_1(t_action_state **state);
-void	reduce_2(t_action_state **state);
-void	reduce_3(t_action_state **state);
-void	reduce_4(t_action_state **state);
-void    reduce_5(t_action_state **state);
-void	reduce_6(t_action_state **state);
-void	reduce_7(t_action_state **state);
-
-
+t_meta			ft_check_meta(char c);
+t_state			ft_check_type(char c);
+t_token_type	choose_type(t_token *new);
+t_token			*new_content(t_lexer *lexer);
+int				ft_next_char(t_lexer *lexer);
+int				ft_norm_to_blank(t_lexer *lexer);
+int				ft_norm_to_sq(t_lexer *lexer);
+int				ft_norm_to_dq(t_lexer *lexer);
+int				ft_norm_to_meta(t_lexer *lexer);
+int				ft_blank_to_norm(t_lexer *lexer);
+int				ft_blank_to_sq(t_lexer *lexer);
+int				ft_blank_to_dq(t_lexer *lexer);
+int				ft_blank_to_meta(t_lexer *lexer);
+int				ft_quote_to_quote(t_lexer *lexer);
+int				ft_meta_to_norm(t_lexer *lexer);
+int				ft_meta_to_blank(t_lexer *lexer);
+int				ft_meta_to_sq(t_lexer *lexer);
+int				ft_meta_to_dq(t_lexer *lexer);
+int				ft_meta_to_meta(t_lexer *lexer);
+void			lexer_setting(t_lexer *lexer, char *input_line);
+int				ft_lexer(t_lexer *lexer);
+void			lexer_setting(t_lexer *lexer, char *input_line);
+int				ft_lexer(t_lexer *lexer);
+void			ft_table(t_lexer *lexer);
+void			reduce_1(t_action_state **state);
+void			reduce_2(t_action_state **state);
+void			reduce_3(t_action_state **state);
+void			reduce_4(t_action_state **state);
+void			reduce_5(t_action_state **state);
+void			reduce_6(t_action_state **state);
+void			reduce_7(t_action_state **state);
 t_action_state	*new_state(void);
-void	linked_state(t_action_state **state, t_action_state **next, t_token **token);
-t_state_num ft_state_0(t_token_type type);
-t_state_num ft_state_1(t_token_type type);
-t_state_num ft_state_2(t_action_state **state, t_token_type type);
-t_state_num ft_state_3(t_action_state **state, t_token_type type);
-t_state_num ft_state_4(t_action_state **state, t_token_type type);
-t_state_num ft_state_5(t_action_state **state, t_token_type type);
-t_state_num ft_state_6(t_token_type type);
-t_state_num ft_state_7(t_token_type type);
-t_state_num ft_state_8(t_action_state **state, t_token_type type);
-t_state_num ft_state_9(t_action_state **state, t_token_type type);
-t_state_num ft_state_10(t_action_state **state, t_token_type type);
-void	ft_goto(t_action_state **state, t_token **token);
-int	ft_parser(t_pipe_list *pipe_head, t_token *token, t_envp_list *env);
-void	ft_set_type(t_token_type *type, t_action_state **state, t_token **token);
-void	ft_enqueue_arg(t_pipe_node **pipe, char *data);
-void	ft_enqueue_redir(t_pipe_node **pipe, t_token_type type, char *data);
-int	ft_is_empty_q_pipe(t_pipe_list *queue);
-void	ft_enqueue_pipe(t_pipe_list **queue);
-t_limiter_q	*new_limiter_q(void);
+void			linked_state(t_action_state **state, t_action_state **next, \
+				t_token **token);
+t_state_num		ft_state_0(t_token_type type);
+t_state_num		ft_state_1(t_token_type type);
+t_state_num		ft_state_2(t_action_state **state, t_token_type type);
+t_state_num		ft_state_3(t_action_state **state, t_token_type type);
+t_state_num		ft_state_4(t_action_state **state, t_token_type type);
+t_state_num		ft_state_5(t_action_state **state, t_token_type type);
+t_state_num		ft_state_6(t_token_type type);
+t_state_num		ft_state_7(t_token_type type);
+t_state_num		ft_state_8(t_action_state **state, t_token_type type);
+t_state_num		ft_state_9(t_action_state **state, t_token_type type);
+t_state_num		ft_state_10(t_action_state **state, t_token_type type);
+void			ft_goto(t_action_state **state, t_token **token);
+int				ft_parser(t_pipe_list *pipe_head, t_token *token, \
+				t_envp_list *env);
+void			ft_set_type(t_token_type *type, t_action_state **state, \
+				t_token **token);
+void			ft_enqueue_arg(t_pipe_node **pipe, char *data);
+void			ft_enqueue_redir(t_pipe_node **pipe, t_token_type type, \
+				char *data);
+int				ft_is_empty_q_pipe(t_pipe_list *queue);
+void			ft_enqueue_pipe(t_pipe_list **queue);
+t_limiter_q		*new_limiter_q(void);
 t_redir_list	*new_redir_list(void);
-t_arg_list	*new_arg_list(void);
-void	ft_arg_id(t_pipe_node **pipe, t_token **token);
-int	ft_arg_redir_in(t_pipe_node **pipe, t_token **token);
-int	ft_arg_redir_out(t_pipe_node **pipe, t_token **token);
-int	ft_arg_redir_append(t_pipe_node **pipe, t_token **token);
-int	ft_arg_heredoc(t_pipe_node **pipe, t_token **token);
-int	ft_arg_pipe(t_token **token);
-void	ft_token_free(t_lexer *lexer);
-
-void	ft_print(t_pipe_list *pipe);
-void	ft_state_free(t_action_state **state);
-void	ft_parser_free(t_pipe_list *pipe);
+t_arg_list		*new_arg_list(void);
+void			ft_arg_id(t_pipe_node **pipe, t_token **token);
+int				ft_arg_redir_in(t_pipe_node **pipe, t_token **token);
+int				ft_arg_redir_out(t_pipe_node **pipe, t_token **token);
+int				ft_arg_redir_append(t_pipe_node **pipe, t_token **token);
+int				ft_arg_heredoc(t_pipe_node **pipe, t_token **token);
+int				ft_arg_pipe(t_token **token);
+void			ft_token_free(t_lexer *lexer);
+void			ft_state_free(t_action_state **state);
+void			ft_parser_free(t_pipe_list *pipe);
 
 #endif
